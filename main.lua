d8babab5578d15cd76f2c1ba40da9262d23f21b3
lluhasigma3 <elenaismagilwa@gmail.com>
2026-09-20 16:14:20 +0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lluhasigma3/LLUHA-HUB/main/core.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lluhasigma3/LLUHA-HUB/main/ui.lua"))()
